5f0e9fd8ae2d372fbc32f61a145e92ce51dba6c7
Irene <gallibour.irene@gmail.com>
2024-07-02 20:02:12 +0200

    modif fichier css et html

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-/////////URL fetch///////////////////////////////////////////////////////////
-//const url = 'http://localhost:3000'
-/////////SIGNUP//////////////////////////////////////////////////////////////
 document.querySelector('#search').addEventListener('click', function () {
     const entries = {
         departure: document.querySelector('#departure').value,
@@ -9,51 +5,33 @@ document.querySelector('#search').addEventListener('click', function () {
         date: document.querySelector('#date').value,
     }
 
-    fetch(`${url}/searchTrip`, {
-        // method: 'POST',
-        // headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify(entries),
-    })
+    if (entries.departure === "" || entries.arrival === "" || entries.date === "") {
+        document.querySelector('#imgReponse').src = './images/notfound.png'
+        document.querySelector('#textReponse').textContent = 'No trip found.'
+    } else {
+        fetch(`http://localhost:3000/searchTrip`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(entries),
+        })
         .then(response => response.json())
         .then(data => {
-            if (data.result) {
-
-                document.querySelector('#imgReponse').style.display = 'none'
-                document.querySelector('#textReponse').style.display = 'none'
-
-                document.querySelector('zoneDeReponse').innerHTML += `
-                    <div class='trajet'>
-                         <div> ${data.departure}> ${data.arrival}</div>
-                        <div>${data.date}</div>
-                          <div>${data.prix}</div>
-                          <button id="book">Book</button>
-                    </div>
-                    `
-            } else {
-                document.querySelector('#imgReponse').src = '/frontend_ticketshacks/images/notfound.png'
-                document.querySelector('#textReponse').textContent = 'No trip found.'
-            }
-        });
-});
-
-////////////////test
-// document.querySelector('#search').addEventListener('mouseover', function () {
-//     document.querySelector('#imgReponse').src = '/frontend_ticketshacks/images/notfound.png'
-//     document.querySelector('#textReponse').textContent = 'No trip found.'
-// })
-
-// document.querySelector('#search').addEventListener('mouseover', function () {
-//     document.querySelector('#imgReponse').style.display = 'none'
-//     document.querySelector('#textReponse').style.display = 'none'
-// })
-
-
-
-document.querySelector('#search').addEventListener('mouseover', function () {
-    const date = document.querySelector('#date').value
-    console.log(date)
-
+            data.trips.forEach(trip =>{
+                let date = trip.date
+                date = moment(date).format('HH:mm'); 
+                document.querySelector('#zoneDeReponse').innerHTML += `
+                        <div class='trajet'>
+                            <div> ${trip.departure} > ${trip.arrival}</div>
+                            <div>${date}</div>
+                            <div>${trip.price}</div>
+                            <button id="book">Book</button>
+                        </div> `
+            
+        })
+    })
+        document.querySelector('#imgReponse').style.display = 'none'
+        document.querySelector('#textReponse').style.display = 'none'
+    }
 })
 
-
-console.log('ttertertert')
+    
